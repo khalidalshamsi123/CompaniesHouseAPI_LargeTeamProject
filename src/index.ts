4f83a5f10cc794edacd5b022bc72341b1223bc8b
Isaac Vieira <vieirai@cardiff.ac.uk>
2024-02-19 16:25:42 +0000

    added lint-fix stage for local use to fix before committing

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
-import * as dotenv from 'dotenv'
-import express from 'express'
+import * as dotenv from 'dotenv';
+import express from 'express';
 // For good security defaults.
-import helmet from 'helmet'
+import helmet from 'helmet';
 
 const app = express();
 dotenv.config();
 
 // Import and use routes.
-import formRoute from './routes/form'
-app.use('/form', formRoute)
+import formRoute from './routes/form';
+app.use('/form', formRoute);
 
 // Configure port and start listening for requests.
-const PORT = process.env.port || 5000
+const PORT = process.env.port || 5000;
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}.`));
+app.listen(PORT, () => {
+	console.log(`Listening on port ${PORT}.`);
+});
