69960d1a8dbba31e4173934c07e1f6105343c381
Jack Richards <richardsj31@cardiff.ac.uk>
2024-02-19 16:25:42 +0000

    Added in the route handler files we plan to do work in. I have done this with the agreement of the team.
    If we created our own versions of each route hanlder file, we risk merge conflicts, therefore this is simpler.

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,22 @@
-import * as dotenv from 'dotenv'
-import express from 'express'
+import * as dotenv from 'dotenv';
+import express from 'express';
 // For good security defaults.
-import helmet from 'helmet'
+import helmet from 'helmet';
 
 const app = express();
 dotenv.config();
 
 // Import and use routes.
-import formRoute from './routes/form'
-app.use('/form', formRoute)
+import approvedRoute from './routes/approved';
+app.use('/approved', approvedRoute);
+
+import submitRoute from './routes/submit';
+app.use('/submit', submitRoute);
+
+import uploadRoute from './routes/upload';
+app.use('/upload', uploadRoute);
 
 // Configure port and start listening for requests.
-const PORT = process.env.port || 5000
+const PORT = process.env.port || 5000;
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}.`));
