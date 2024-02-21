227caf08eb0ba854065d005be537d829ebffff70
c21086065 <alshamsik@cardiff.ac.uk>
2024-02-21 12:39:11 +0000

    I made a start with user story 10. I created a sub route from "/approved", "/approved/hmrc". The initial step was just to get a response. With that done, i made a function that reads csv files using polars. To test it out, i made the response send the entire csv file, in json to /approved/hmrc.

--- src/routes/approved.ts
+++ src/routes/approved.ts
@@ -1,8 +1,22 @@
 import { Router } from 'express'
+import pl from 'nodejs-polars'
 const router = Router()
 
 router.get('/', (req, res) => {
   res.send("Success").status(200)
 })
+
+//reads csv files
+function csvReader(csvFile: string){
+  let json_csv = pl.readCSV(csvFile);
+  return json_csv;
+}
+
+//creating a sub route
+router.get('/hmrc', (req, res) => {
+ //res.send("HMRC page").status(200)
+  res.send(csvReader("hmrc-supervised-data-test-data.csv"))
+} )
+
 
 export default router
