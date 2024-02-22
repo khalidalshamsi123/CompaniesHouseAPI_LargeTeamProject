f949dd4061585ec41a46aeb381832365ee7a0741
c21086065 <alshamsik@cardiff.ac.uk>
2024-02-22 22:01:58 +0000

    With this commit, User story 10 is almost done. I have now managed to read the csv file, extract data from two specific columns and return the values of a business name and its status.

--- src/routes/approved.ts
+++ src/routes/approved.ts
@@ -1,24 +1,39 @@
 import { Router } from 'express'
-import pl, { col } from 'nodejs-polars'
+import pl, { DataFrame, col } from 'nodejs-polars'
 const router = Router()
 
 router.get('/', (req, res) => {
-  res.send("Success").status(200)
+  res.send('Success').status(200);
 })
 
 
-function csvReader(csvFile: string, columnName1: string, columnName2: string){
+function csvReader(csvFile: string, columnName1: string, columnName2: string, targetValue: string){
   //reads the file
   let csvData = pl.readCSV(csvFile);
+
   //filters the response with two columns
-  let filteredData = csvData.select(columnName1, columnName2);
-  return filteredData;
+  let columnOneValues = csvData.getColumn(columnName1);
+  let columnTwoValues = csvData.getColumn(columnName2);
+
+  let index = 0;
+  //looping to find the index of a specific value
+  for(let i = 0; i < columnOneValues.length; i++){
+    if(columnOneValues[i] === targetValue){
+      index = i;
+      break;
+    }
+  }
+
+  //Grouping the two values in one variable.
+  let specificValue = columnOneValues[index] + columnTwoValues[index];
+
+  return specificValue;
 }
 
 //creating a sub route
 router.get('/hmrc', (req, res) => {
-  res.send(csvReader("hmrc-supervised-data-test-data.csv", 'BUSINESS_NAME', 'STATUS1')).status(200);
+  res.send(csvReader('hmrc-supervised-data-test-data.csv', 'BUSINESS_NAME', 'STATUS1', 'HARRY SMITH AND SONS')).status(200);
 } )
 
 
-export default router
+export default router;
