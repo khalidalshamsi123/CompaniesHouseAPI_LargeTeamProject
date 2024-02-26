2162af127f5f8dda88894f86d59fc524febd6161
c21086065 <alshamsik@cardiff.ac.uk>
2024-02-26 13:45:27 +0000

    another small mistake with the description of the test.

--- src/__tests__/APPROVED_HMRC.test.ts
+++ src/__tests__/APPROVED_HMRC.test.ts
@@ -5,7 +5,7 @@ import app from '../app';
 describe('Given Companies House wants to retrieve the approval status of GWYN DEBBSON AND DAUGHTER from the HMRC CSV file.', () => {
 	// When.
 	describe('When Companies House sends a request to the /approved/hmrc endpoint.', () => {
-		it('Then it would return the business name and the status applied.', async () => {
+		it('Then it would return the business name and the status approved.', async () => {
 			// Make the request and wait for the response and expect status code 200
 			const response = await request(app).get('/approved/hmrc').expect(200);
 			// Assert the response
