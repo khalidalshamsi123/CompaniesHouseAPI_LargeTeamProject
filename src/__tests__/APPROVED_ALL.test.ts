10b86e855d1e18c6a9befa39969a479e77d32ecb
Isaac Vieira <vieirai@cardiff.ac.uk>
2024-03-04 13:09:31 +0000

    final fix to test to ensure business name is returned as it was not being given in the psuedo request before

--- src/__tests__/APPROVED_ALL.test.ts
+++ src/__tests__/APPROVED_ALL.test.ts
@@ -18,12 +18,12 @@ describe('Setup Database - Creating business_registry table for the following te
     describe('Given a request is made to retrieve approval status for a specific registration ID from the /allApproved endpoint.', () => {
         describe('When the registration ID 122702 is provided to the endpoint.', () => {
             it('Then the response should contain FCA authorization as true and HMRC and Gambling Commission authorization as false.', async () => {
-                // Make the request and wait for the response
+                //Make the request and wait for the response
                 const response = await request(app)
                     .get('/approved/allApproved')
-                    .query({ registrationId: '122702' });
+                    .query({ registrationId: '122702', businessName: 'Barclays' });
 
-                // Assert the response
+                //Assert the response
                 expect(response.status).toBe(200);
                 expect(response.body).toHaveProperty('registrationId', '122702');
                 expect(response.body).toHaveProperty('businessName');
