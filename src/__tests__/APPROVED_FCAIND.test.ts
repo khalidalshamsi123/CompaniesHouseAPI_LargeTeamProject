1c59496f66a698c4f74723c2d1712f22551be96d
c21087406 <alsarmih@cardiff.ac.uk>
2024-03-06 12:48:26 +0000

    Justr added description to the tests, havent started but this was old work as this user story will be pushed aside, due clients purposes

--- src/__tests__/APPROVED_FCAIND.test.ts
+++ src/__tests__/APPROVED_FCAIND.test.ts
@@ -1,5 +1,13 @@
 import request from 'supertest';
 import app from '../app';
+import isAuthorised from '../routes/approved';
 
 // Scenario: FCA endpoint returns 400 then logic is exuted for FCA individual.
-
+// Given
+describe('Given a firm is not found or unathorised in the Firm API'), () => {
+	// When
+	describe('The status comes back as FSR-API-02-01-11'), () => {
+		// Then
+		it()
+	};
+};
