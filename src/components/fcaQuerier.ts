7b1871cc65666f6e98a8cd42a4cdc51279c42fbd
c21087406 <alsarmih@cardiff.ac.uk>
2024-03-08 13:05:52 +0000

    Advancement in user story #24 added the three error hadnling methods with the correct status codes, however i haevent added any tests yet to it. I am a bit confused in the layout of the entrie strucutre of the code as the aggregator did switch some logic and code snippites. To overcome this I will dicuss this change further with Jack and Isacc.

--- src/components/fcaQuerier.ts
+++ src/components/fcaQuerier.ts
@@ -21,7 +21,25 @@ const axiosConfig: AxiosRequestConfig = {
 // Since if we handle it, we would need to return a boolean which would be incorrect. As just because
 // the API request may have failed, that doesn't mean the business itself is unapproved.
 async function fcaGetApprovalStatus(registrationId: string): Promise<{isAuthorised: boolean}> {
+	// Check if registrationId contains only numerical characters
+	if (!/^\d+$/.test(registrationId)) {
+		throw new Error('FSR-API-02-01-11: Bad Request - Invalid Input for registrationId');
+	}
+
+	// Check if 'X-Auth-Email' and 'X-Auth-Key' headers are present and have the correct values
+	if (
+		(!axiosConfig.headers || !axiosConfig.headers['X-Auth-Email'] || !axiosConfig.headers['X-Auth-Key'])
+		|| (axiosConfig.headers['X-Auth-Email'] !== 'vieirai@cardiff.ac.uk' || !process.env.API_KEY_FCA)
+	) {
+		throw new Error('FSR-API-01-01-11 Unauthorised: Please include a valid API key and Email address');
+	}
+
 	const fcaResponse = await axios.get(`https://register.fca.org.uk/services/V0.1/Firm/${registrationId}`, axiosConfig);
+
+	if (fcaResponse.data.Message === 'ERROR : Account Not Found') {
+		// Respond with a specific error message for account not found
+		throw new Error('FSR-API-02-01-21: ERROR : Account Not Found');
+	}
 
 	let isAuthorised = false;
 	// The response could be null which can throw error when we try to read it for the authorised value.
