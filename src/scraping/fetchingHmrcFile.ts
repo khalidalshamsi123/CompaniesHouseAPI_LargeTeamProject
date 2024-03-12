f7b1d1baafd9456d206eef356047654b9a021549
c21086065 <alshamsik@cardiff.ac.uk>
2024-03-12 12:24:05 +0000

    Progress towards user story 28. Still using CSS selectors, my code now locates the href which contains the csv url. Moreover, i have added a 'downloadCsvFile' function which uses axios to fetch the href url then create a csv file with the fetched data. This very much completes the functions needed for the user story. However, i would like to use the x-path to traverse the DOM tree rather than CSS selectors. This was brought up because of the fear that the div elements can change in the future. Using the x-path to traverse the nodes was suggested by my supervisor.

--- src/scraping/fetchingHmrcFile.ts
+++ src/scraping/fetchingHmrcFile.ts
@@ -1,15 +1,19 @@
 import axios, {type AxiosResponse} from 'axios';
 import * as cheerio from 'cheerio';
-// Loading the hmrc website
+import * as fs from 'fs';
 
-// Selecting the element 'body'.
-// const $body = $('body');
+async function downloadCsvFile(hrefLink: string) {
+	try {
+		const hrefResponse: AxiosResponse<ArrayBuffer> = await axios.get(hrefLink, {responseType: 'arraybuffer'});
 
-// Traversing the divs searching for grandchildren divs
-// $('body #wrapper #content .govuk-grid-row .govuk-grid-column-two-thirds #contents .gem-c-govspeak.govuk-govspeak .govspeak ')
-// 	.each((index, element) => {
-// 		console.log($(element));
-// 	});
+		const fileData = Buffer.from(hrefResponse.data);
+		fs.writeFile('./testhmrcfile.csv', fileData, () => {
+			console.log('csv file downloaded!');
+		}); // Save as a CSV file
+	} catch (error) {
+
+	}
+}
 
 async function scrapeHmrcWebsite() {
 	try {
@@ -18,9 +22,16 @@ async function scrapeHmrcWebsite() {
 
 		const $ = cheerio.load(hmrcResponse.data);
 
-		$('body #wrapper #content .govuk-grid-row .govuk-grid-column-two-thirds #contents .gem-c-govspeak.govuk-govspeak .govspeak p .gem-c-attachment-link')
-			.each((index, element) => {
-				console.log($(element).text());
+		// Traversing through the html elements
+		$('body #wrapper #content .govuk-grid-row .govuk-grid-column-two-thirds #contents .gem-c-govspeak.govuk-govspeak .govspeak p .gem-c-attachment-link .govuk-link')
+			.each(async (index, element) => {
+				// Find the href
+				const href = $(element).attr('href');
+				// Check if the file is of type csv
+				if (href && href.endsWith('.csv')) {
+					console.log(href);
+					await downloadCsvFile(href);
+				}
 			});
 	} catch (error) {
 		console.error('Error occurred during scraping:', error);
