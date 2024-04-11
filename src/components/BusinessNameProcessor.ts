540955d1f516df99492da888ee75a5c1433016d0
c21052928 <richardsj31@cardiff.ac.uk>
2024-04-11 19:57:14 +0100

    Completes #42 functionally. I renamed the Business Name Standardizer class to Business Name Processor. As I intend for it to be a more general class to handle tasks relating to business names.
    In that class, I implemented a method that takes a business name (typically the one provided by the user), and the reference ID associated with a row under a regulatory body.
    
    It then fetches the row in our records using this ID, and compares the given business name against the one associated with the record.
    
    The provided name is standardized before this comparison is made. If they do not match this is reflected in the response the method gives. It will also provide a error message showcases the difference in this instance.
    
    Otherwise, it will return 'true' indicating a match.
    
    I plan to use this method within the aggregator to provide more context to the client if the business name doesn't exactly match in our systems.

---
+++ src/components/BusinessNameProcessor.ts
@@ -0,0 +1,113 @@
+import pool from '../database/setup/databasePool';
+
+export default class BusinessNameProcessor {
+	/** A hashmap of the legal structures found in the UK.
+	 * 	The short-hand and long-form version of these legal structures tend to be used interchangebly.
+	 *  Making it important that we standardize them to a common format their long-form.
+	 *
+	 *  I made design decision to convert from abbreviated to unabbreviated form because of a few reasons:
+	 *     1. The unabbreviated version is much more likely to be found within the business name itself.
+	 *	      Meaning it would be difficult to assess whether we have detected the legal structure of the business, or a part of its name.
+	 *	      E.g., `Game Company Corporation Ltd`. would be standardized as `Game Company Corp Ltd`. which isn't valid or intended.
+	 *	   2. We can safely assume that an abbreviation will be its own 'word', that it will be seperated from other words by spaces.
+	 *	  	  Which in turn enables us to more easily detect them and acquire the right unabbreviated version to replace them with.
+	*/
+	private readonly legalStructures = new Map<string, string>([
+		['ltd', 'limited'],
+		['llp', 'limited liability partnership'],
+		['plc', 'public limited company'],
+		['inc', 'incorporated'],
+		['corp', 'corporation'],
+		['lp', 'limited partnership'],
+		['swrl', 'society with restricted liability'],
+		['assoc', 'association'],
+		['cic', 'community interest company'],
+	]);
+
+	/**
+	 * Standardizes an array of business names. Lower-cases each, and converts any known abbreviated legal structures in the name to the unabbreviated version.
+	 * @param {string[]} businessNames An array of business name strings.
+	 * @returns {string[]} An array of standardized business names.
+	 */
+	public standardize(businessNames: string[]): string;
+	/**
+	 * Standardizes a business name, sets it to lower-case and converts any known abbreviated legal structures to the unabbreviated version.
+	 * @param businessName Business name to standardize.
+	 * @returns {string} A standardized version of the provided business name.
+	 */
+	public standardize(businessName: string): string;
+
+	public standardize(nameData: string | string[]): string | string[] {
+		if (nameData instanceof Array) {
+			// Standardize each business name and return result in an array.
+			// This keeps the output structured the same as the input.
+			return nameData.map(name => this.standardizeSingle(name));
+		}
+
+		return this.standardizeSingle(nameData);
+	}
+
+	/**
+	 * Compares a given business name with the corresponding name in the database based on the specified regulator.
+ 	 * This method fetches the business name from a specific regulatory table (either HMRC or Gambling) using the provided record ID,
+     * standardizes the provided business name using `standardizeSingle`, and checks for a match.
+	 * @param {string} recordID - The unique identifier for the business record in the database.
+ 	 * @param {string} businessName - The business name to compare against the database.
+ 	 * @param {'hmrc' | 'gambling'} regulator - The regulator type, which determines the database schema and table. Valid values are 'hmrc' or 'gambling'.
+ 	 * @returns {Promise<{isMatch: boolean, message?: string}>} A promise that resolves to an object containing:
+ 	 *    - `isMatch`: A boolean indicating whether the provided business name matches the name in the database.
+ 	 *    - `message`: An optional message describing the result of the comparison. This message is included if there is a mismatch.
+ 	 * @throws {Error} Throws an error if the business cannot be found, or if other issues occur.
+	 */
+	public async compareBusinessNameWithRecord(recordID: string, businessName: string, regulator: 'hmrc' | 'gambling'): Promise<{isMatch: boolean; message?: string}> {
+		let schema = 'registration_schema';
+		if (process.env.NODE_ENV === 'test') {
+			schema = 'test_schema';
+		}
+
+		let table;
+		switch (regulator) {
+			case 'hmrc':
+				table = 'hmrc_business_registry';
+				break;
+			case 'gambling':
+				table = 'gambling_business_registry';
+				break;
+			// Can't remove default case even though it's redundant because another lint rule will then throw an error.
+			// eslint-disable-next-line @typescript-eslint/switch-exhaustiveness-check
+			default:
+				// Do nothing.
+		}
+
+		businessName = this.standardizeSingle(businessName);
+
+		const data = await pool.query(`SELECT businessname FROM ${schema}.${table} WHERE referenceid = $1`, [recordID]);
+
+		if (data.rowCount === 0 || !data.rows[0].businessname) {
+			throw new Error('Business not found in our records under the given regulatory body.');
+		}
+
+		const businessNameDatabase = data.rows[0].businessname as string;
+
+		if (businessNameDatabase === businessName) {
+			// Names match.
+			return {isMatch: true};
+		}
+
+		const message = `Provided standardized business name mistmaches with the one in our records. Given: ${businessName}, Expected: ${businessNameDatabase}`;
+		return {isMatch: false, message};
+	}
+
+	private standardizeSingle(name: string): string {
+		name = name.toLowerCase();
+		for (const [abbreviated, unabbreviated] of this.legalStructures.entries()) {
+			if (name.includes(abbreviated)) {
+				const regex = new RegExp(`\\b${abbreviated}\\b`, 'gi');
+				name = name.replace(regex, unabbreviated);
+			}
+			// Any unrecognised unabbreviated legal structures are left unchanged.
+		}
+
+		return name;
+	}
+}
