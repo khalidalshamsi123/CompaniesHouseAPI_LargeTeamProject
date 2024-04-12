5587a14e748b1b19c8976394540bcff2c83e3c6d
c21086065 <alshamsik@cardiff.ac.uk>
2024-04-12 13:12:46 +0100

    Added a JSDOC comment for the scraping method and made a baseURL variable out of the loop that would be used for the gambling commission file.

--- src/scraping/fetchingFiles.ts
+++ src/scraping/fetchingFiles.ts
@@ -2,7 +2,14 @@ import axios, {type AxiosResponse} from 'axios';
 import * as cheerio from 'cheerio';
 import * as fs from 'fs';
 import {convertToCsv} from './odsToCsv';
-
+/**
+ *
+ * @param hrefLink
+ * @param fileName
+ * @returns fileData
+ * This method downloads the files by writing new files.
+ * This method uses axios to retrieve the data.
+ */
 async function downloadCsvFile(hrefLink: string, fileName: string) {
 	try {
 		const hrefResponse: AxiosResponse<ArrayBuffer> = await axios.get(hrefLink, {responseType: 'arraybuffer'});
@@ -18,6 +25,20 @@ async function downloadCsvFile(hrefLink: string, fileName: string) {
 	}
 }
 
+/**
+ *
+ * @param elementsPath
+ * @param websiteLink
+ * @returns returnedHref
+ * This method scrapes the websites bt traversing through the css selectors passed to it
+ * 'elementsPath'.
+ * The methods has 4 if statements, each for different files. The first two are for the gambling commission
+ * csv files. The problem with the gambling commission website is that when its scraped, the href of the div
+ * that contains the csv files isnt the full URL which is why i had to hardcode the baseURL and add it to the retrieved href.
+ * As for the HMRC website i had to make two different if conditions one for if it was a csv file, which was the case initially,
+ * and the other condition is if the file was an ODS file, which is the file type they provide now in which i would have to convert to csv.
+ * The reason it was kept is for the uncertainty of the fact that the file type might change back to csv again.
+ */
 async function scrapeWebsite(elementsPath: string, websiteLink: string) {
 	try {
 		// This is disabled because i need the type to be buffer for it to work.
@@ -26,6 +47,7 @@ async function scrapeWebsite(elementsPath: string, websiteLink: string) {
 
 		const $ = cheerio.load(response.data);
 		let returnedHref = 'empty';
+		const gamblingBaseUrl = 'https://www.gamblingcommission.gov.uk';
 
 		const elements = $(elementsPath);
 
@@ -35,13 +57,13 @@ async function scrapeWebsite(elementsPath: string, websiteLink: string) {
 			let href = $(element).attr('href');
 			// Checks if the file ends with businesses.csv, this is exclusive to gambling commission.
 			if (href?.endsWith('businesses.csv')) {
-				href = 'https://www.gamblingcommission.gov.uk' + href;
+				href = gamblingBaseUrl + href;
 				returnedHref = href;
 				console.log(returnedHref);
 				await downloadCsvFile(href, './business-licence-register-businesses.csv');
 			// This checks if the href ends with licences.csv and is exclusive to the license gambling commission file.
 			} else if (href?.endsWith('licences.csv')) {
-				href = 'https://www.gamblingcommission.gov.uk' + href;
+				href = gamblingBaseUrl + href;
 				returnedHref = href;
 				console.log(returnedHref);
 				await downloadCsvFile(href, './business-licence-register-licences.csv');
