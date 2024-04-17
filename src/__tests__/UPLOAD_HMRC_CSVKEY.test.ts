e0d3191bd558ae6889470547516b151847edfe8c
Isaac Vieira <vieirai@cardiff.ac.uk>
2024-04-17 16:57:22 +0100

    Fixed second test in UPLOAD_HMRC_CSVKEY, was using fail and promise handling incorrectly

--- src/__tests__/UPLOAD_HMRC_CSVKEY.test.ts
+++ src/__tests__/UPLOAD_HMRC_CSVKEY.test.ts
@@ -24,24 +24,13 @@ describe('HMRC Component', () => {
 
 	describe('Given the hmrcCsv key is incorrect', () => {
 		beforeEach(() => {
-			// Set up mock implementation for fs.existsSync
 			(fs.existsSync as jest.Mock).mockReturnValue(false);
 		});
 
 		it('Then it should raise an error', async () => {
-			try {
-				// Call the uploadHmrcCsv function with invalid csvKey
-				const uploader = new HmrcStandardiser();
-				const csvKeys = ['nothmrcCsv' as CsvKeys];
-				expect(uploader.standardise(csvKeys, 'test_schema').catch(console.error));
-				// If the function does not throw an error, fail the test
-				fail('Function should have thrown an error.');
-			} catch (error) {
-				// Assert that the error is an instance of Error before accessing its message property
-				expect(error).toBeInstanceOf(Error);
-				// Expect the error message to indicate that the CSV file was not found
-				expect((error as Error).message).toContain('Invalid csvKey provided. Expected "hmrcCsv"');
-			}
+			const uploader = new HmrcStandardiser();
+			const csvKeys = ['nothmrcCsv' as CsvKeys];
+			await expect(uploader.standardise(csvKeys, 'test_schema')).rejects.toThrow('Invalid csvKey provided. Expected "hmrcCsv"');
 		});
 	});
 });
